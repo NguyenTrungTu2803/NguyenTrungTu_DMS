ae1caf8488558c710124a80e65d6ef5538d55a63
NguyenTrungTu2803 <54448405+NguyenTrungTu2803@users.noreply.github.com>
2020-06-21 22:00:04 +0700

    update

---
+++ NguyenTrungTu_DMS/DAO/BorderTextbox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DAO
+{
+    public class BorderTextbox : TextBox
+    {
+        //TextBox textBox;
+
+        public Color DefaultBorderColor { get; set; }
+        public Color FocusedBorderColor { get; set; }
+
+        public override string Text
+        {
+            get { return this.Text; }
+            set { this.Text = value; }
+        }
+        public BorderTextbox()
+        {
+            BackColor = Color.White;
+            this.Width = 150; this.Height = 50;
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            BackColor = Color.White;
+            base.OnEnter(e);
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            BackColor = SystemColors.ControlDark;
+            base.OnLeave(e);
+        }
+    }
+}
---
+++ NguyenTrungTu_DMS/GUI/Login.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace GUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.borderTextbox2 = new DAO.BorderTextbox();
+            this.borderTextbox1 = new DAO.BorderTextbox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(699, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(737, 120);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(201, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(404, 52);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TRƯỜNG ĐẠI HỌC CÔNG NGHIỆP\r\n            THỰC PHẨM TP.HCM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(230, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phần mềm quản ký văn bản";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(260, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tên đăng nhập ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(313, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Mật khẩu";
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = global::GUI.Properties.Resources._160317icon_login;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel3.Location = new System.Drawing.Point(24, 148);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(188, 219);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::GUI.Properties.Resources.unnamed;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(178, 120);
+            this.panel2.TabIndex = 1;
+            // 
+            // borderTextbox2
+            // 
+            this.borderTextbox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.borderTextbox2.DefaultBorderColor = System.Drawing.SystemColors.ControlDark;
+            this.borderTextbox2.FocusedBorderColor = System.Drawing.Color.Red;
+            this.borderTextbox2.Location = new System.Drawing.Point(420, 221);
+            this.borderTextbox2.Name = "borderTextbox2";
+            this.borderTextbox2.Size = new System.Drawing.Size(246, 22);
+            this.borderTextbox2.TabIndex = 10;
+            // 
+            // borderTextbox1
+            // 
+            this.borderTextbox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.borderTextbox1.DefaultBorderColor = System.Drawing.SystemColors.ControlDark;
+            this.borderTextbox1.FocusedBorderColor = System.Drawing.Color.Red;
+            this.borderTextbox1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.borderTextbox1.Location = new System.Drawing.Point(420, 161);
+            this.borderTextbox1.Name = "borderTextbox1";
+            this.borderTextbox1.Size = new System.Drawing.Size(246, 25);
+            this.borderTextbox1.TabIndex = 11;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 450);
+            this.Controls.Add(this.borderTextbox1);
+            this.Controls.Add(this.borderTextbox2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private DAO.BorderTextbox borderTextbox2;
+        private DAO.BorderTextbox borderTextbox1;
+    }
+}
+
