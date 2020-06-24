21b2c1e83538bc92e66d6f39585ff28197b80170
NguyenTrungTu2803 <54448405+NguyenTrungTu2803@users.noreply.github.com>
2020-06-24 16:17:23 +0700

    update

---
+++ NguyenTrungTu_DMS/DAO/buttonDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAO
+{
+    public class buttonDAO : Button
+    {
+        public Color DefaultBorderColor { get; set; }
+        public Color FocusedBorderColor { get; set; }
+
+        public override string Text
+        {
+            get { return this.Text; }
+            set { this.Text = value; }
+        }
+        public buttonDAO()
+        {
+            BackColor = Color.DarkViolet;
+            this.Width = 50; this.Height = 30;
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            BackColor = Color.White;
+            base.OnEnter(e);
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            BackColor = SystemColors.ControlDark;
+            base.OnLeave(e);
+        }
+    }
+}
