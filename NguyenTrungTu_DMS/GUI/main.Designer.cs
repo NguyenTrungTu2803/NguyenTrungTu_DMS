2126811c52971d06b48cc5a38fc2fcb968d5058b
NguyenTrungTu2803 <54448405+NguyenTrungTu2803@users.noreply.github.com>
2020-06-29 21:38:08 +0700

    update

--- NguyenTrungTu_DMS/GUI/main.Designer.cs
+++ NguyenTrungTu_DMS/GUI/main.Designer.cs
@@ -31,14 +31,14 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -48,6 +48,7 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -62,39 +63,6 @@
             this.panel1.Size = new System.Drawing.Size(1189, 98);
             this.panel1.TabIndex = 0;
             // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(1, 97);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1190, 55);
-            this.flowLayoutPanel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel2.Controls.Add(this.button10);
-            this.panel2.Controls.Add(this.button9);
-            this.panel2.Controls.Add(this.button8);
-            this.panel2.Controls.Add(this.button7);
-            this.panel2.Controls.Add(this.button6);
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(0, 152);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(212, 625);
-            this.panel2.TabIndex = 2;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(256, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(934, 625);
-            this.panel3.TabIndex = 3;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -106,12 +74,47 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "PHẦN MỀM QUẢN LÝ VĂN BẢN";
             // 
-            // panel5
+            // panel4
             // 
-            this.panel5.Location = new System.Drawing.Point(316, 191);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(845, 518);
-            this.panel5.TabIndex = 3;
+            this.panel4.BackgroundImage = global::GUI.Properties.Resources.unnamed;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel4.Location = new System.Drawing.Point(1, 2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 98);
+            this.panel4.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(1, 97);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1190, 36);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.button11);
+            this.panel2.Controls.Add(this.button10);
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(0, 135);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(212, 913);
+            this.panel2.TabIndex = 2;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "home_prod_icon-1.png");
             // 
             // button1
             // 
@@ -122,27 +125,26 @@
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.ImageKey = "home_prod_icon-1.png";
             this.button1.ImageList = this.imageList1;
-            this.button1.Location = new System.Drawing.Point(-3, -2);
+            this.button1.Location = new System.Drawing.Point(-2, -2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(212, 47);
+            this.button1.Size = new System.Drawing.Size(212, 86);
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // panel4
+            // panel3
             // 
-            this.panel4.BackgroundImage = global::GUI.Properties.Resources.unnamed;
-            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel4.Location = new System.Drawing.Point(1, 2);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(200, 98);
-            this.panel4.TabIndex = 0;
+            this.panel3.Location = new System.Drawing.Point(256, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(934, 625);
+            this.panel3.TabIndex = 3;
             // 
-            // imageList1
+            // panel5
             // 
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "home_prod_icon-1.png");
+            this.panel5.Location = new System.Drawing.Point(256, 159);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(921, 893);
+            this.panel5.TabIndex = 3;
             // 
             // button2
             // 
@@ -153,9 +155,9 @@
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.ImageKey = "home_prod_icon-1.png";
             this.button2.ImageList = this.imageList1;
-            this.button2.Location = new System.Drawing.Point(0, 289);
+            this.button2.Location = new System.Drawing.Point(-2, 326);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(212, 47);
+            this.button2.Size = new System.Drawing.Size(212, 86);
             this.button2.TabIndex = 5;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
@@ -169,9 +171,9 @@
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button3.ImageKey = "home_prod_icon-1.png";
             this.button3.ImageList = this.imageList1;
-            this.button3.Location = new System.Drawing.Point(8, 297);
+            this.button3.Location = new System.Drawing.Point(-2, 408);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(212, 47);
+            this.button3.Size = new System.Drawing.Size(212, 86);
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = false;
@@ -185,9 +187,9 @@
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button4.ImageKey = "home_prod_icon-1.png";
             this.button4.ImageList = this.imageList1;
-            this.button4.Location = new System.Drawing.Point(16, 305);
+            this.button4.Location = new System.Drawing.Point(-2, 653);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(212, 47);
+            this.button4.Size = new System.Drawing.Size(212, 86);
             this.button4.TabIndex = 7;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = false;
@@ -201,9 +203,9 @@
             this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button5.ImageKey = "home_prod_icon-1.png";
             this.button5.ImageList = this.imageList1;
-            this.button5.Location = new System.Drawing.Point(24, 313);
+            this.button5.Location = new System.Drawing.Point(-2, 816);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(212, 47);
+            this.button5.Size = new System.Drawing.Size(212, 86);
             this.button5.TabIndex = 8;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = false;
@@ -217,9 +219,9 @@
             this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button6.ImageKey = "home_prod_icon-1.png";
             this.button6.ImageList = this.imageList1;
-            this.button6.Location = new System.Drawing.Point(32, 321);
+            this.button6.Location = new System.Drawing.Point(-2, 243);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(212, 47);
+            this.button6.Size = new System.Drawing.Size(212, 86);
             this.button6.TabIndex = 9;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = false;
@@ -233,9 +235,9 @@
             this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button7.ImageKey = "home_prod_icon-1.png";
             this.button7.ImageList = this.imageList1;
-            this.button7.Location = new System.Drawing.Point(40, 329);
+            this.button7.Location = new System.Drawing.Point(-2, 80);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(212, 47);
+            this.button7.Size = new System.Drawing.Size(212, 86);
             this.button7.TabIndex = 10;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = false;
@@ -249,9 +251,9 @@
             this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button8.ImageKey = "home_prod_icon-1.png";
             this.button8.ImageList = this.imageList1;
-            this.button8.Location = new System.Drawing.Point(48, 337);
+            this.button8.Location = new System.Drawing.Point(-2, 162);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(212, 47);
+            this.button8.Size = new System.Drawing.Size(212, 86);
             this.button8.TabIndex = 11;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = false;
@@ -265,9 +267,9 @@
             this.button9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button9.ImageKey = "home_prod_icon-1.png";
             this.button9.ImageList = this.imageList1;
-            this.button9.Location = new System.Drawing.Point(56, 345);
+            this.button9.Location = new System.Drawing.Point(-2, 490);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(212, 47);
+            this.button9.Size = new System.Drawing.Size(212, 86);
             this.button9.TabIndex = 12;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = false;
@@ -281,18 +283,34 @@
             this.button10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button10.ImageKey = "home_prod_icon-1.png";
             this.button10.ImageList = this.imageList1;
-            this.button10.Location = new System.Drawing.Point(0, 398);
+            this.button10.Location = new System.Drawing.Point(-2, 571);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(212, 47);
+            this.button10.Size = new System.Drawing.Size(212, 86);
             this.button10.TabIndex = 13;
             this.button10.Text = "button10";
             this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button11.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.White;
+            this.button11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button11.ImageKey = "home_prod_icon-1.png";
+            this.button11.ImageList = this.imageList1;
+            this.button11.Location = new System.Drawing.Point(-2, 735);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(212, 86);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = false;
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1189, 777);
+            this.ClientSize = new System.Drawing.Size(1189, 1055);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -318,6 +336,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button8;
